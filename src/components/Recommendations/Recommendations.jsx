e76a33bb22cf1d5f0899490ac71ac16c5f746702
Your Name <you@example.com>
2023-01-12 13:20:28 -0600

    Creating intial file structure for app

---
+++ src/components/Recommendations/Recommendations.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './recommendations.css';
+
+const Recommendations = () => {
+  return (
+    <div>Recommendations</div>
+  )
+}
+
+export default Recommendations
