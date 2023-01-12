0b927fbafefae12438bdaf6427cf9fa9f660d2fa
Your Name <you@example.com>
2023-01-12 13:32:44 -0600

    Importing all the files into App.jsx so I can render those components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+
 import './index.css';
+
+//Importing all the files needed to render the components
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+import About from './components/About/About';
+import Experience from './components/Experience/Experience';
+import Projects from './components/Projects/Projects';
+import Recommendations from './components/Recommendations/Recommendations';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
  
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Projects />   
+      <Recommendations />  
+      <Contact />
+      <Footer /> 
+    </>
   )
 }
 
