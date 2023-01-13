43f6754bfc4f125d0ef2c83f4f92075afd0c3e44
Your Name <you@example.com>
2023-01-13 15:18:46 -0600

    Finished styling the header and added media queries

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,6 @@ const Header = () => {
   return (
    <header>
       <div className='container header__container'>
-        <h5>Hey I'm</h5>
         <h1>Leah Choco</h1>
         <h5 className='text-light'>FullStack Developer</h5>
         <CTA />
@@ -20,8 +19,6 @@ const Header = () => {
         <div className='me'>
           <img src={ME} alt='me'/>
         </div>
-
-        <a href='#contact' className='scroll__down'>Scroll Down</a>
       </div>
 
    </header>
