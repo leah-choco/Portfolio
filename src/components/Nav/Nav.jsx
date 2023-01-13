e98ba504aed50ef8d9950fdc0a5df3b9192a0dd8
Your Name <you@example.com>
2023-01-13 15:37:19 -0600

    Importing icons for my Navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import {FaHome} from 'react-icons/fa';
+import{BsFillPersonLinesFill} from 'react-icons/bs';
+import{ GiBookshelf} from 'react-icons/gi';
+import{GoTools} from 'react-icons/go';
+import{FaRegThumbsUp} from 'react-icons/fa';
+import{AiOutlineMail} from 'react-icons/ai';
 import './nav.css';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><FaHome /></a>
+      <a href="#about"><BsFillPersonLinesFill /></a>
+      <a href="#experience"><GiBookshelf /></a>
+      <a href="#projects"><GoTools /></a>
+      <a href="#recommendations"><FaRegThumbsUp /></a>
+      <a href="#contact"><AiOutlineMail /></a>
+    </nav>
   )
 }
 
