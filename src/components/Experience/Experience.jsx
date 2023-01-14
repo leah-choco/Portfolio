9773b57d70284b88fe383c0e4c7b79e219e9b449
Your Name <you@example.com>
2023-01-14 14:43:15 -0600

    Finished styling the Experiences component and moved h4s into divs

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -13,29 +13,41 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className='experience__content'>
             <article className='experience__details'>
-              <AiOutlineRightCircle />
-              <h4>HTML</h4>
+              <AiOutlineRightCircle className='experience__details-icon' />
+                <div>
+                 <h4>HTML</h4>
+                </div>
             </article>
             <article className='experience__details'>
-              <AiOutlineRightCircle />
-              <h4>CSS</h4>
+              <AiOutlineRightCircle className='experience__details-icon'/>
+                <div>
+                  <h4>CSS</h4>
+                </div>
             </article>
             <article className='experience__details'>
-              <AiOutlineRightCircle />
-              <h4>Javascript</h4>
+              <AiOutlineRightCircle className='experience__details-icon'/>
+                <div>
+                  <h4>Javascript</h4>
+                </div>
             </article>
             <article className='experience__details'>
-              <AiOutlineRightCircle />
-              <h4>CSS Frameworks</h4>
-              <small className='text-light'>Bootstrap, Bulma, MUI</small>
+              <AiOutlineRightCircle className='experience__details-icon'/>
+                <div>
+                  <h4>CSS Frameworks</h4>
+                  <small className='text-light'>Bootstrap, Bulma, MUI</small>
+                </div>
             </article>
             <article className='experience__details'>
-              <AiOutlineRightCircle />
-              <h4>Handlebars</h4>
+              <AiOutlineRightCircle className='experience__details-icon'/>
+                <div>
+                  <h4>Handlebars</h4>
+                </div>
             </article>
             <article className='experience__details'>
-              <AiOutlineRightCircle />
-              <h4>React</h4>
+              <AiOutlineRightCircle className='experience__details-icon'/>
+                <div>
+                 <h4>React</h4>
+                </div>
             </article>
           </div>
         </div>
@@ -43,28 +55,40 @@ const Experience = () => {
           <h3>Backend Development</h3>
             <div className='experience__content'>
               <article className='experience__details'>
-                <AiOutlineRightCircle />
-                <h4>Express.js</h4>
+                <AiOutlineRightCircle className='experience__details-icon'/>
+                  <div>
+                   <h4>Express.js</h4>
+                  </div>
               </article>
               <article className='experience__details'>
-                <AiOutlineRightCircle />
-                <h4>Node.js</h4>
+                <AiOutlineRightCircle className='experience__details-icon'/>
+                  <div>
+                   <h4>Node.js</h4>
+                  </div>
               </article>
               <article className='experience__details'>
-                <AiOutlineRightCircle />
-                <h4>MySQL</h4>
+                <AiOutlineRightCircle className='experience__details-icon'/>
+                  <div>
+                    <h4>MySQL</h4>
+                  </div>
               </article>
               <article className='experience__details'>
-                <AiOutlineRightCircle />
-                <h4>MongoDB</h4>
+                <AiOutlineRightCircle className='experience__details-icon'/>
+                  <div>
+                    <h4>MongoDB</h4>
+                  </div>
               </article>
               <article className='experience__details'>
-                <AiOutlineRightCircle />
-                <h4>Apollo GraphQL</h4>
+                <AiOutlineRightCircle className='experience__details-icon'/>
+                  <div>
+                    <h4>Apollo GraphQL</h4>
+                  </div>
               </article>
               <article className='experience__details'>
-                <AiOutlineRightCircle />
-                <h4>RESTful APIs</h4>
+                <AiOutlineRightCircle className='experience__details-icon'/>
+                  <div>
+                    <h4>RESTful APIs</h4>
+                  </div>
               </article>
           </div>    
         </div>
