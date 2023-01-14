3af255f28adac77cf3c459c4af1b7d1a374ac5c4
Your Name <you@example.com>
2023-01-14 12:08:29 -0600

    Creating About Me jsx with icons

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import ME from '../../assets/Leah Laughing Portfolio.jpeg'
+import{TbCertificate} from 'react-icons/tb';
+import {GoLocation} from 'react-icons/go';
+import{GiStack} from 'react-icons/gi';
+
 
 import './about.css';
 
 const About = () => {
   return (
-   <section id='about'>About</section>
+   <section id='about'>
+    <h2>About Me</h2>
+
+    <div className='container about__container'>
+      <div className='about__me'>
+        <div className='about__me-image'>
+          <img src={ME} alt='Leah Laughing'/>
+        </div>
+      </div>
+
+      <div className='about__content'>
+        <div className='about__cards'></div>
+          <article className='about__card'>
+            <TbCertificate className='about__icon' />
+            <h5>Education</h5>
+            <small>Vanderbilt University Certification</small>
+          </article>
+
+          <article className='about__card'>
+            <GoLocation className='about__icon' />
+            <h5>Location</h5>
+            <small>Middle Tennessee</small>
+          </article>
+
+          <article className='about__card'>
+            <GiStack className='about__icon' />
+            <h5>MERN Stack</h5>
+            <small>MongoDB, Express, React, Node</small>
+          </article>
+      </div>
+
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis, excepturi? 
+        Eaque, neque illo? Dolorum eius numquam tenetur praesentium voluptates et quo fugit expedita illo distinctio, 
+        dicta explicabo sapiente consequatur debitis!
+        </p>
+    </div>
+   </section>
   )
 }
 
--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href='https://docs.google.com/document/d/1-B0Bd3GqD-P8bBREHyThjBUX6MdnrExAtxKIVBB67Yw/edit?usp=sharing' className='btn'>See Resume</a>
+        <a href='https://docs.google.com/document/d/1-B0Bd3GqD-P8bBREHyThjBUX6MdnrExAtxKIVBB67Yw/edit?usp=sharing' target='_blank' className='btn'>See Resume</a>
         <a href='#contact' className='btn btn-primary'>Contact Me</a>
     </div>
 
