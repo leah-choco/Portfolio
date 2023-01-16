2d51fcb07aa40485839e2ecf30504997f8e2296c
Your Name <you@example.com>
2023-01-16 14:08:01 -0600

    Working on structure for projects, will change to be more dynamic later

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,67 @@
 import React from 'react';
 
 import './projects.css';
+//import athletetracker from '../../assets/athletetracker'
+import GoalE from '../../assets/Screen Shot 2022-11-15 at 11.38.23 AM.png';
+
 
 const Projects = () => {
   return (
-    <section id='projects'>Projects</section>
+    <section id='projects'>
+      <h2>Portfolio</h2>
+
+      <div className='container project__container'>
+        <article className='project__item'>
+          <div className='portfolio__item-image'>
+            <img src={GoalE} alt=''/>
+          </div>
+          <h3>athlete-tracker</h3>
+          <a href='https://github.com/leah-choco/Grand-Finale' className='btn' target='_blank'>Github</a>
+          <a href='' className='btn btn-primary' target='_blank'>Deployed Link</a>
+        </article>
+        <article className='project__item'>
+          <div className='portfolio__item-image'>
+            <img src={GoalE} alt=''/>
+          </div>
+          <h3>athlete-tracker</h3>
+          <a href='https://github.com/leah-choco/Grand-Finale' className='btn' target='_blank'>Github</a>
+          <a href='' className='btn btn-primary' target='_blank'>Deployed Link</a>
+        </article>
+        <article className='project__item'>
+          <div className='portfolio__item-image'>
+            <img src={GoalE} alt=''/>
+          </div>
+          <h3>athlete-tracker</h3>
+          <a href='https://github.com/leah-choco/Grand-Finale' className='btn' target='_blank'>Github</a>
+          <a href='' className='btn btn-primary' target='_blank'>Deployed Link</a>
+        </article>
+        <article className='project__item'>
+          <div className='portfolio__item-image'>
+            <img src={GoalE} alt=''/>
+          </div>
+          <h3>athlete-tracker</h3>
+          <a href='https://github.com/leah-choco/Grand-Finale' className='btn' target='_blank'>Github</a>
+          <a href='' className='btn btn-primary' target='_blank'>Deployed Link</a>
+        </article>
+        <article className='project__item'>
+          <div className='portfolio__item-image'>
+            <img src={GoalE} alt=''/>
+          </div>
+          <h3>athlete-tracker</h3>
+          <a href='https://github.com/leah-choco/Grand-Finale' className='btn' target='_blank'>Github</a>
+          <a href='' className='btn btn-primary' target='_blank'>Deployed Link</a>
+        </article>
+        <article className='project__item'>
+          <div className='portfolio__item-image'>
+            <img src={GoalE} alt=''/>
+          </div>
+          <h3>athlete-tracker</h3>
+          <a href='https://github.com/leah-choco/Grand-Finale' className='btn' target='_blank'>Github</a>
+          <a href='' className='btn btn-primary' target='_blank'>Deployed Link</a>
+        </article>
+
+      </div>
+    </section>
   )
 }
 
