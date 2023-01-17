66a9d8d1a66f8fe312544d9cfb6c2639eb60193d
Your Name <you@example.com>
2023-01-17 14:38:02 -0600

    Writing jsx for the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,20 @@ import'./footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>Leah Choco</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About Me</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#projects'>Projects</a></li>
+        <li><a href='#references'>References</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      
+    </footer>
   )
 }
 
