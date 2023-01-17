9d9d0e8842df720d99998a7060d31102136f560d
Your Name <you@example.com>
2023-01-17 14:12:33 -0600

    Writing jsx for Contact Me component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 import './contact.css';
 
+import{AiOutlineMail} from 'react-icons/ai';
+
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <AiOutlineMail />
+            <h4>Email</h4>
+            <h5>mrsleahchoco@gmail.com</h5>
+            <a href='mailto:mrsleahchoco@gmail.com' target='_blank' rel='noreferrer'>Send me an email!</a>
+          </article>
+        </div>
+      </div>
+    </section>
   )
 }
 
