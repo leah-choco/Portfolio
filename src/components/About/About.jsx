5baf91d97be9cea3d3e87c8d204ad638eca9f120
Your Name <you@example.com>
2023-01-18 18:22:11 -0600

    Changing about components to be clickable, not just icons

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -22,21 +22,15 @@ const About = () => {
       <div className='about__content'>
         <div className='about__cards'>
           <article className='about__card'>
-          <a href='https://www.linkedin.com/in/leah-choco-70531b245/' target='_blank' rel='noreferrer'className='about__icon'><BsLinkedin/></a>
-            <h5>LinkedIn</h5>
-            <small>Click the icon to see my LinkedIn profile.</small>
+          <a href='https://www.linkedin.com/in/leah-choco-70531b245/' target='_blank' rel='noreferrer'className='about__icon'><BsLinkedin/>LinkedIn</a>
           </article>
 
           <article className='about__card'>
-          <a href='https://github.com/leah-choco' target='_blank' rel='noreferrer' className='about__icon'><FaGithubSquare /></a>
-            <h5>Github</h5>
-            <small>Click the icon to see my Github Repos.</small>
+          <a href='https://github.com/leah-choco' target='_blank' rel='noreferrer' className='about__icon'><FaGithubSquare />Github</a>
           </article>
 
           <article className='about__card'>
-          <a href='https://www.facebook.com/profile.php?id=100009366021089' target='_blank' rel='noreferrer' className='about__icon'><BsFacebook /></a>
-            <h5>Facebook</h5>
-            <small>Click the icon to see my Facebook profile.</small>
+          <a href='https://www.facebook.com/profile.php?id=100009366021089' target='_blank' rel='noreferrer' className='about__icon'><BsFacebook />Facebook</a>
           </article>
         </div>  
         <p>I am currently located in Middle Tennessee with my husband and three children.
