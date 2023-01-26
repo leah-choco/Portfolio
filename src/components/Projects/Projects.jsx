5b8cc19b2deda9d6f075986306d59b3bf0deb291
Your Name <you@example.com>
2023-01-26 10:40:43 -0600

    Adding description of projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,6 +11,7 @@ const data = [
     id: 1,
     image: athletetracker,
     title: 'athletetracker',
+    description: 'Allows a personal trainer to track their athletes, update, and delete them. Contains options for waiver and payment. Uses full MERN stack with Apollo.',
     github: 'https://github.com/leah-choco/athletetracker',
     link: 'https://athletetracker.herokuapp.com/'
   },
@@ -18,6 +19,7 @@ const data = [
     id: 2,
     image: GoalE,
     title: 'Goal-E',
+    description:'Allows the user to login in and create goals. Uses CRUD to keep goals current as the user needs to update or delete goals. Uses MySQL and Handlebars.',
     github: 'https://github.com/leah-choco/Goal-E',
     link: ' https://evening-stream-22967.herokuapp.com/'
   },
@@ -25,6 +27,7 @@ const data = [
     id: 3,
     image: forecast,
     title: 'Weather Forecast',
+    description:'Allows the user to search for a city to see current weather specfics. Uses HTML, CSS, JavaScript, weather API, and localStorage.',
     github: 'https://github.com/leah-choco/Weather-Forecast',
     link:'https://leah-choco.github.io/Weather-Forecast/'
   },
@@ -32,6 +35,7 @@ const data = [
     id: 5,
     image: schedule,
     title: 'Work Day Schedule',
+    description:'Allows user to create an hourly schedule. Color coding is used to highlight past, current, and future. Uses HTML, CSS, and JavaScript.',
     github:'https://github.com/leah-choco/Work-Day-Schedule',
     link:' https://leah-choco.github.io/Work-Day-Schedule/'
   }
@@ -46,13 +50,14 @@ const Projects = () => {
 
       <div className='container project__container'>
         {
-          data.map(({ id, image, title, github, link}) =>{
+          data.map(({ id, image, title, description, github, link}) =>{
             return (
               <article key={id} className='project__item'>
               <div className='project__item-image'>
                 <img src={image} alt={title}/>
               </div>
               <h3>{title}</h3>
+              <small>{description}</small>
               <div className='project__item-cta'>
                 <a href={github} className='btn btn-primary' target='_blank' rel='noreferrer'>Github Repo</a>
                 <a href={link} className='btn btn-primary' target='_blank' rel='noreferrer'>Live Demo</a>
